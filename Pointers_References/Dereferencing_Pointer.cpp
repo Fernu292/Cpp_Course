4e5581fd4349e0076f661e881cfde7ef98598910
Fernando <fernu292@gmail.com>
2024-01-03 03:21:29 -0600

    Add Pointers Excersices

--- Pointers_References/Dereferencing_Pointer.cpp
+++ Pointers_References/Dereferencing_Pointer.cpp
@@ -7,21 +7,48 @@
 */
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 
 int main(){
+    cout<<"\n-----------------------------"<<endl; 
 
     int score(100);
     int *score_ptr = &score;
 
-    cout<<"Direction of score: "<<score_ptr<<endl; //Address
+    cout<<"\nDirection of score: "<<score_ptr<<endl; //Address
     cout<<"Value of score from the pointer: "<<*score_ptr<<endl; //100
 
     *score_ptr = 200;
     cout<<"New score: "<<*score_ptr<<endl;//200
     cout<<"New score: "<<score<<endl; //200
+
+    cout<<"\n-----------------------------"<<endl; 
+    
+    double high_temp (100.7);
+    double low_temp (37.4);
+    double *temp_ptr = &high_temp;
+
+    cout<<"\nHigh Temp: "<<*temp_ptr<<endl;
+
+    temp_ptr = &low_temp;
+
+    cout<<"Low Temp: "<<*temp_ptr<<endl;
+
+    cout<<"\n-----------------------------"<<endl; 
+
+    string name ("Fernando");
+    string *string_ptr = &name;
+
+    cout<<"\nName by ptr: "<<*string_ptr<<endl;
+    name = "Luis";
+    cout<<"Changed name: "<<*string_ptr<<endl;
+
+    cout<<"\n-----------------------------"<<endl; 
+
 
     return 0;
 }
